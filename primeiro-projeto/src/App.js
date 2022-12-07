22fa87f24a25142e351fde84e78ba59f476a3581
Joel da Rocha Laranjeira Junior <joellaranjeira@gmail.com>
2022-12-07 19:13:51 -0300

    correção

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to TESTE.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Joel
+          Learn React
         </a>
       </header>
     </div>
