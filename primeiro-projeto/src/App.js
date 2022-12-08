22feacee9d5a4b7079fc3ba9e2c7119b6b2cbe56
joellaranjeira <118441245+joellaranjeira@users.noreply.github.com>
2022-12-07 22:28:13 -0300

    aula:3

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Joel'
+  const newName = name.toUpperCase()
+
+  function sum ( a,b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - Alterado aqui!!
-        </a>
-      </header>
-    </div>
-  );
+   <div className = "App">
+
+    <h2>Olá {newName}</h2>
+    <h1>A soma é: {sum(3,9)}</h1>
+    <img src={url} alt="Minha imagem"/>
+
+   </div>
+  )
 }
 
 export default App;
