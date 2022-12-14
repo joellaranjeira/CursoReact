db5a3c9b7cb5cbf6a528a31b03b0373a6dc41e73
Joel da Rocha Laranjeira Junior <joellaranjeira@gmail.com>
2022-12-14 08:35:10 -0300

    aula 8

--- primeiro-projeto/src/components/List.js
+++ primeiro-projeto/src/components/List.js
@@ -1,14 +1,24 @@
 import Item from './Item'
+import ItemProps from './ItemProps'
 function List ()
 {
     return(
     <>
-        <p>Minha List</p>
+        <h1>Minha List</h1>
         <ul>
-            <li>Item 01</li>
-            <li>Item 02</li>
-            <Item marca = "Fragments 01"/>
-            <Item marca = "Fragments 02"/>
+            <p>List</p>
+            <li>Gol</li>
+            <li>Uno</li>
+
+            <p>Item</p>
+            <Item marca = "Jeep"/>
+            <Item marca = "Fox"/>
+
+            <p>ItemProps</p>
+            <ItemProps marca="Fusca" ano_lancamento = {1979}/>
+            <ItemProps marca="Pollo" ano_lancamento = {1979}/> 
+            <ItemProps />
+
         </ul>
     </>
     )
