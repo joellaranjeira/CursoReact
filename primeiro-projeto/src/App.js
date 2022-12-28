c8edb62c9367b494139425772c6c9c3281bdf36f
Joel da Rocha Laranjeira Junior <joellaranjeira@gmail.com>
2022-12-28 12:25:36 -0300

    aula 15 final

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import NavBar from './components/layout/NavBar'
 import Contato from './components/pages/Contato'
@@ -7,21 +7,20 @@ import Home from './components/pages/Home'
 
 function App() {
   return (
-    <Router>
-      <NavBar/>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route path="/empresa">
-          <Empresa/>
-        </Route>
-        <Route path="/contato">
-          <Contato/>
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' exact='true' element={<Home/>}></Route>
+            <Route path='/empresa' element={<Empresa/>}></Route>
+            <Route path='/contato' element={<Contato/>}></Route>
+          </Routes>
+       
+          <Footer />
+        </Router>
+      </header>
+    </div>
   )
 }
 
