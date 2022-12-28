2005372097837465149f9326f51f9f77960e9e26
Joel da Rocha Laranjeira Junior <joellaranjeira@gmail.com>
2022-12-28 12:27:49 -0300

    ajustes aula 15 final

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import NavBar from './components/layout/NavBar'
 import Contato from './components/pages/Contato'
@@ -12,11 +12,11 @@ function App() {
         <Router>
           <NavBar />
           <Routes>
-            <Route path='/' exact='true' element={<Home/>}></Route>
-            <Route path='/empresa' element={<Empresa/>}></Route>
-            <Route path='/contato' element={<Contato/>}></Route>
+            <Route path='/' exact='true' element={<Home />}></Route>
+            <Route path='/empresa' element={<Empresa />}></Route>
+            <Route path='/contato' element={<Contato />}></Route>
           </Routes>
-       
+
           <Footer />
         </Router>
       </header>
